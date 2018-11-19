9b4176767247c42ac107b8fb7ba996c5b67bb6d9
tianxin459 <onlyheart@qq.com>
2018-11-19 17:42:48 +0800

    configuration

---
+++ BulletMessage/Contract/Request/ConfigureRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BulletMessage.Contract.Request
+{
+    public class ConfigureRequest
+    {
+        public string ConfigurationJson { get; set; }
+    }
+}
---
+++ BulletMessage/Controllers/ClientController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using BulletMessage.Contract.Request;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulletMessage.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        public static string ConfigurationJson { get; set; }
+        [HttpGet]
+        [Route("config")]
+        public IActionResult GetConfig()
+        {
+            var path = Directory.GetCurrentDirectory() + @"\config";
+            StreamReader sr = new StreamReader(path);
+            List<string> strOutput = new List<string>();
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+                strOutput.Add(line);
+            }
+            return Ok();
+        }
+
+
+        [HttpPost]
+        [Route("config")]
+        public IActionResult SetConfig(ConfigureRequest request)
+        {
+            ConfigurationJson = request.ConfigurationJson;
+            var path = Directory.GetCurrentDirectory()+@"\config";
+
+            FileStream fs = new FileStream(path, FileMode.Create);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.Write(request.ConfigurationJson);
+            sw.Flush();
+            sw.Close();
+            fs.Close();
+            return Ok();
+        }
+    }
+}
